ba13052ca0521509aadf0ac4c2362dada5b43ee9
CassianoMari <mariana.rafaela.silva@escola.pr.gov.br>
2026-09-25 09:28:49 -0300

    aula1

---
+++ script.js
@@ -0,0 +1,6 @@
+let btnAjuda = document.querySelector(".botao-ajuda");
+let btnFechar =  document.querySelector(".botaofechar");
+let modal =  document.querySelector(".modal-fundo");
+
+btnAjuda.addEventListener("click", abreModal);
+btnAjuda.addEventListener("click", fechaModal);
